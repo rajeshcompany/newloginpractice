f94444f389a8f030c74198f2264d41ade2ea982d
rajesh <rajesh@mycompany.com>
2022-08-23 00:11:21 +0530

    files moving to repo

---
+++ src/test/java/funnyproject/AppTest.java
@@ -0,0 +1,20 @@
+package funnyproject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
